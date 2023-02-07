f8017fd9e4cc25633d74a76c1156eecc62b82559
Mursyid <91861596+mursyidd@users.noreply.github.com>
2023-02-07 08:43:27 +0800

    first

---
+++ HashtableDictionaryMaui/Employee.cs
@@ -0,0 +1,11 @@
+﻿/* Exemplary file for Chapter 4 - Dictionaries and Sets. */
+
+namespace HashtableDictionaryMaui
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
--- HashtableDictionaryMaui/MainPage.xaml.cs
+++ HashtableDictionaryMaui/MainPage.xaml.cs
@@ -5,78 +5,71 @@ namespace HashtableDictionaryMaui;
 
 public partial class MainPage : ContentPage
 {
-	public Hashtable phoneBook = new Hashtable();
-	public Dictionary<string, string> products = new Dictionary<string, string>();
+	//public Hashtable UserHash = new Hashtable();
+	//public Dictionary<int, Employee> UserDict = new Dictionary<int, Employee>();
 
 
     public MainPage()
 	{
 		InitializeComponent();
 
-		//Hashtable phoneBook = new Hashtable()
-		//{
-		//	{ "Marcin Jamro", "000-000-000" },
-		//	{ "John Smith", "111-111-111" }
-		//};
 
-		//Dictionary<string, string> products = new Dictionary<string, string>
-		//{
-		//	{ "5900000000000", "A1" },
-		//	{ "5901111111111", "B5" },
-		//	{ "5902222222222", "C9" }
-		//};
-	}
+        editorHash.Text = "Hashtable testing = new Hashtable()";
+        editorHash.Text += Environment.NewLine;
+        editorHash.Text += Environment.NewLine;
+        editorHash.Text += (
+            "        testing.Add(\"1\", \"Mursyid\");", Environment.NewLine +
+            "        testing.Add(\"2\", \"Syawani\");", Environment.NewLine +
+            "        testing.Add(\"3\", \"Akmal\");", Environment.NewLine +
+            "        testing.Add(\"4\", \"Marni\");");
+        editorHash.Text += Environment.NewLine;
+        editorHash.Text += Environment.NewLine;
+        editorHash.Text += "Output:-";
+        editorHash.Text += Environment.NewLine;
+
+        Hashtable testing = new Hashtable();
+        testing.Add("1", "Mursyid");
+        testing.Add("2", "Syawani");
+        testing.Add("3", "Akmal");
+        testing.Add("4", "Marni");
+
+        foreach (DictionaryEntry e in testing)
+        {
+            editorHash.Text += string.Format("ID:{0,5}  Staff:{1,20}{2}", e.Key, e.Value, Environment.NewLine);
+
+        }
+
+
+        editorDict.Text = "Dictionary<string, string> testing2 = new Dictionary<string, string>()";
+        editorDict.Text += Environment.NewLine;
+        editorDict.Text += Environment.NewLine;
+        editorDict.Text += (
+    "        testing2.Add(\"1\", \"Mursyid\");", Environment.NewLine +
+    "        testing2.Add(\"2\", \"Syawani\");", Environment.NewLine +
+    "        testing2.Add(\"3\", \"Akmal\");", Environment.NewLine +
+    "        testing2.Add(\"4\", \"Marni\");");
+        editorDict.Text += Environment.NewLine;
+        editorDict.Text += Environment.NewLine;
+        editorDict.Text += "Output:-";
+        editorDict.Text += Environment.NewLine;
+
+        Dictionary<string, string> testing2 = new Dictionary<string, string>();
+        testing2.Add("1", "Mursyid");
+        testing2.Add("2", "Syawani");
+        testing2.Add("3", "Akmal");
+        testing2.Add("4", "Marni");
+
+        foreach (KeyValuePair<string,string> e in testing2)
+        {
+            //string tesss = string.Format("ID: {0} Staff: {1}", e.Key.ToString(), e.Value.ToString());
+
+            editorDict.Text += string.Format("ID:{0,5}  Staff:{1,20}{2}", e.Key, e.Value, Environment.NewLine);
+
+        }
+    }
 
     private void Hashbtn_Clicked(object sender, EventArgs e)
     {
-		string inputText1 = entHash1.Text;
-        string inputText2 = entHash2.Text;
-        int? inputInt1 = 0;
-        int? inputInt2 = 0;
-		bool check1 = false;
-		bool check2 = false;
-
-        try
-        {
-            inputInt1 = Convert.ToInt32(inputText1);
-			if (inputInt1.HasValue && inputInt1 != 0)
-				check1 = true;
-        }
-		catch (Exception)
-		{
-		}
-
-		try
-		{
-            inputInt2 = Convert.ToInt32(inputText2);
-            if (inputInt2.HasValue && inputInt2 != 0)
-                check2 = true;
-        }
-		catch (Exception)
-		{
-		}
-
-		if (check1)
-		{
-			if (check2)
-			{
-                phoneBook.Add(inputInt1, inputInt2);
-            }
-            else
-			{
-                phoneBook.Add(inputInt1, inputText2);
-            }
-        }
-		else if (check2)
-		{
-            phoneBook.Add(inputText1, inputInt2);
-        }
-        else
-		{
-            phoneBook.Add(inputText1, inputText2);
-        }
-
-		
     }
 
     private void Dictbtn_Clicked(object sender, EventArgs e)
